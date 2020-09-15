cf356cf55121c72061b03318bdfed0c315198660
daizihao <IloveSJX1314!~~>
2020-09-15 21:11:13 +0800

    update

--- kudu11xwriter/src/main/java/com/q1/datax/plugin/writer/kudu11xwriter/Key.java
+++ kudu11xwriter/src/main/java/com/q1/datax/plugin/writer/kudu11xwriter/Key.java
@@ -15,15 +15,15 @@ public class Key {
     public static final String TYPE = "type";
     public static final String INDEX = "index";
     public static final String PRIMARYKEY = "primaryKey";
-    public static final String COMPRESSION = "compression";
+    public static final String COMPRESSION = "compress";
     public static final String COMMENT = "comment";
     public final static String ENCODING = "encoding";
 
 
 
-    public static final String NUM_REPLICAS = "numReplicas";
+    public static final String NUM_REPLICAS = "replicaCount";
     public static final String HASH = "hash";
-    public static final String HASH_NUM = "num";
+    public static final String HASH_NUM = "number";
 
     public static final String RANGE = "range";
     public static final String LOWER = "lower";
@@ -33,10 +33,10 @@ public class Key {
 
     public static  final String TRUNCATE = "truncate";
 
-    public static final String INSERT_MODE = "insertMode";
+    public static final String INSERT_MODE = "writeMode";
 
-    public static  final String WRITE_BATCH_SIZE = "writeBatchSize";
+    public static  final String WRITE_BATCH_SIZE = "batchSize";
 
-    public static  final String MUTATION_BUFFER_SPACE = "mutationBufferSpace";
+    public static  final String MUTATION_BUFFER_SPACE = "bufferSize";
     public static  final String SKIP_FAIL = "skipFail";
 }
