946be2b3f4f344623cdde2d84cdb369d63d3c861
daizihao <IloveSJX1314!~~>
2020-09-16 17:04:33 +0800

    update

--- kudu11xwriter/src/main/java/com/q1/datax/plugin/writer/kudu11xwriter/Kudu11xWriterErrorcode.java
+++ kudu11xwriter/src/main/java/com/q1/datax/plugin/writer/kudu11xwriter/Kudu11xWriterErrorcode.java
@@ -7,16 +7,16 @@ import com.alibaba.datax.common.spi.ErrorCode;
  * @create 2020-08-27 19:25
  **/
 public enum Kudu11xWriterErrorcode implements ErrorCode {
-    REQUIRED_VALUE("Kuduwriter-00", "您缺失了必须填写的参数值."),
-    ILLEGAL_VALUE("Kuduwriter-01", "您填写的参数值不合法."),
-    GET_KUDU_CONNECTION_ERROR("Kuduwriter-02", "获取Kudu连接时出错."),
-    GET_KUDU_TABLE_ERROR("Kuduwriter-03", "获取 Kudu  table时出错."),
-    CLOSE_KUDU_CONNECTION_ERROR("Kuduwriter-04", "关闭Kudu连接时出错."),
-    CLOSE_KUDU_SESSION_ERROR("Kuduwriter-06", "关闭Kudu table时时出错."),
-    PUT_KUDU_ERROR("Kuduwriter-07", "写入Kudu时发生IO异常."),
-    DELETE_KUDU_ERROR("Kuduwriter-08", "delete Kudu表时发生异常."),
-    GREATE_KUDU_TABLE_ERROR("Kuduwriter-09", "创建 Kudu  table时出错."),
-    PARAMETER_NUM_ERROR("Kuduwriter-10","参数个数不匹配")
+    REQUIRED_VALUE("Kuduwriter-00", "You are missing a required parameter value."),
+    ILLEGAL_VALUE("Kuduwriter-01", "You fill in the parameter values are not legitimate."),
+    GET_KUDU_CONNECTION_ERROR("Kuduwriter-02", "Error getting Kudu connection."),
+    GET_KUDU_TABLE_ERROR("Kuduwriter-03", "Error getting Kudu table."),
+    CLOSE_KUDU_CONNECTION_ERROR("Kuduwriter-04", "Error closing Kudu connection."),
+    CLOSE_KUDU_SESSION_ERROR("Kuduwriter-06", "Error closing Kudu table connection."),
+    PUT_KUDU_ERROR("Kuduwriter-07", "IO exception occurred when writing to Kudu."),
+    DELETE_KUDU_ERROR("Kuduwriter-08", "An exception occurred while delete Kudu table."),
+    GREATE_KUDU_TABLE_ERROR("Kuduwriter-09", "Error creating Kudu table."),
+    PARAMETER_NUM_ERROR("Kuduwriter-10","The number of parameters does not match.")
     ;
 
     private final String code;
